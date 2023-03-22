1b2652c6c5aadf0c3bccf805838e117ed82be26b
Eduardo Rocha <edu.isys@gmail.com>
2023-03-22 00:17:22 -0300

    Criação da Tabela PalavraChave

---
+++ MinhaApiCore/Migrations/20230322031614_AddPalavraChave.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinhaApiCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPalavraChave : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PalavraChave",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    categoria = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PalavraChave", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PalavraChave");
+        }
+    }
+}
